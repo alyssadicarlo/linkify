9f4dbaee1b97dbff5be814723f0d8aaf58787fce
Nate Lee <nleepercussion@gmail.com>
2021-07-06 12:57:14 -0400

    Added links and users models with necessary methods

---
+++ models/Links.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const db = require('./conn');
+
+
+class LinksModel {
+    constructor(id, userID, uuid, custom_link, target_url, title, date_added, click_count) {
+        this.id = id;
+        this.userID = userID;
+        this.uuid = uuid;
+        this.custom_link = custom_link;
+        this.target_url = target_url;
+        this.title = title;
+        this.date_added = date_added;
+        this.click_count = click_count;
+    }
+
+    static async getAll(userID) {
+        try {
+            const response = await db.any(
+                `SELECT * FROM links
+                WHERE userID = ${userID}
+                ORDER by id;`
+            )
+            return response;
+        } catch(error) {
+            console.error("ERROR: ", error);
+            return error;
+        }
+    }
+
+    static async addLink(userID, uuid, custom_link, target_url, title) {
+        try {
+            const query = `
+                INSERT INTO links 
+                    (userID, uuid, custom_link, target_url, title)
+                VALUES
+                (${userID}, '${uuid}', '${custom_link}', '${target_url}', '${title}');`
+            const response = await db.one(query);
+            return response;
+        } catch(error) {
+            console.error("ERROR: ", error);
+            return error;
+        }
+    }
+
+    static async updateLink(id, custom_link, target_url, title) {
+        try {
+            const query = `
+                UPDATE links
+                SET custom_link = '${custom_link}'
+                SET target_url = '${target_url}'
+                SET title = '${title}'
+                WHERE id = ${id};`
+            const response = await db.result(query);
+            return response;
+        } catch(error) {
+            console.error("ERROR: ", error);
+            return error;
+        }
+    }
+
+    static async updateClicks(id) {
+        try {
+            const response = await db.result(`
+                UPDATE links
+                SET click_count = click_count + 1
+                WHERE id = ${id};`
+            )
+        return response;
+
+        } catch(error) {
+            console.error("ERROR: ", error);
+            return error;
+        }
+    }
+
+    static async deleteLink(id) {
+        try {
+            const response = await db.result(
+                `DELETE FROM links
+                WHERE id = ${id};`
+            )
+            return response;
+        } catch(error) {
+            console.log("ERROR: ", error);
+        }
+    }
+}
+
+module.exports = LinksModel;
